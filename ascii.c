6616b0f6ac591c0341af95f6a90a5aa955b0d165
hexmanta <2027zakir.ady@fcboe.org>
2024-01-16 13:34:11 -0500

    Completed mario assignment + ascii art

---
+++ ascii.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("    /Y       /Y      \n");
+	printf("   /  Y     /  Y     \n");
+	printf("  / /Y Y___/ /Y Y    \n");
+	printf("  |              |   \n");
+	printf("  |   O      O   |   \n");
+	printf("  |      VV      |   \n");
+	printf("  Y______________/   \n");
+	printf("   |____________|    \n");
+	return 0;
+}
